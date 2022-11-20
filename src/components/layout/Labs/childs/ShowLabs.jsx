7256166dbeabe8cc969bb67f4b3f66a176a58acb
kimchan122 <chanchankim122@gmail.com>
2022-11-21 03:51:27 +0900

    labs page

--- src/components/layout/Labs/childs/ShowLabs.jsx
+++ src/components/layout/Labs/childs/ShowLabs.jsx
@@ -3,44 +3,93 @@ import LabsTree from '../../../../assets/LabsTree.png';
 import Grid1 from '../../../../assets/Grid1.png';
 import Grid2 from '../../../../assets/Grid2.png';
 import Grid3 from '../../../../assets/Grid3.png';
+import MushroomAdult from '../../../../assets/MushroomAdult.png';
 
 const ShowLabs = () => {
 
-    const ShowSpores = () => {
-        <div>
+    const ItemElements = () => {
+        return (
+            <div className="LabsItemBox">
+                <div className="LabsItemImage"></div>
+                <div className="LabsItemAmount">0</div>
+            </div>
+        )
+    }
 
-        </div>
+    const DummyElements = () => {
+        return (
+            <div className="LabsDummyItemBox"></div>
+        )
+    }
+
+    const ShowSpores = () => {
+        return (
+            <div className="ShowSporesBox">
+                <div className="ItemHorizontalBox">
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                </div>
+                <div className="ShowItemTitle">Spores</div>
+            </div>
+        )
     }
 
     const ShowMushrooms = () => {
-        <div>
-            
-        </div>
+        return (
+            <div className="ShowMushroomsBox">
+                <div className="ShowItemTitle">Mushrooms</div>
+                <div className="ItemHorizontalBox">
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                </div>
+            </div>
+        )
     }
 
     const ShowSpSpores = () => {
-        <div>
-            
-        </div>
+        return (
+            <div className="ShowSpSporesBox">
+                <div className="ShowItemTitle">SPECIAL SPORES</div>
+                <div className="ItemVerticalBox">
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {DummyElements()}
+                </div>
+            </div>
+        )
     }
 
     const ShowSpMushrooms = () => {
-        <div>
-            
-        </div>
+        return (
+            <div className="ShowSpMushroomsBox">
+                <div className="ShowItemTitle">SPECIAL MUSHROOMS</div>
+                <div className="ItemVerticalBox">
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {ItemElements()}
+                    {DummyElements()}
+                </div>
+            </div>
+        )
     }
 
     return (
         <>
             <div className="LabsBox">
-                <div>
-                    <img src={LabsTree} />
-                    <div>
-                        <div>
-                            <img src={Grid2} />
-                        </div>
-                    </div>
-                </div>
+                {ShowSpores()}
+                {ShowMushrooms()}
+                {ShowSpSpores()}
+                {ShowSpMushrooms()}
+                <img src={LabsTree} />
             </div>
         </>
 
