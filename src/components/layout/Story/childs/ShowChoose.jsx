c77b7c6abe34ca320e362d83c84cd8391e836756
kimchan122 <chanchankim122@gmail.com>
2022-11-20 15:31:36 +0900

    init2

--- src/components/layout/Story/childs/ShowChoose.jsx
+++ src/components/layout/Story/childs/ShowChoose.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import L_Bg_Mush from '../../../../assets/L_BackgroundMushrooms.png';
+import L_Bg_Thief from '../../../../assets/L_BackgroundThief.png';
+import R_Bg_Mush from '../../../../assets/R_BackgroundMushrooms.png';
+import R_Bg_Thief from '../../../../assets/R_BackgroundThief.png';
+import MushroomAdult from '../../../../assets/MushroomAdult.png';
+import Thief from '../../../../assets/Thief.png';
 
-const ShowChoose = ({handleFlow}) => {
-    return(
-        <div>
-        </div>
+const ShowChoose = ({ handleFlow }) => {
+    return (
+        <>
+            <div className="StoryChooseBox">
+                <div className="StoryChooseTitle">CHOOSE WHAT DO YOU WANT TO BE</div>
+                <div className="StoryChooseElementBox">
+                    <div>
+                        <div className="StoryImageBox">
+                            <img src={MushroomAdult} />
+                        </div>
+                        <div>
+                            <button className="StoryButtonBox">MUSHRROM</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="StoryImageBox">
+                            <img src={Thief} />
+                        </div>
+                        <div>
+                            <button className="StoryButtonBox">THIEF</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="L_BG">
+                <img src={L_Bg_Mush} />
+                <img src={L_Bg_Thief} className="L_BG_Thief" />
+            </div>
+            <div className="R_BG">
+                <img src={R_Bg_Mush} />
+                <img src={R_Bg_Thief} className="R_BG_Thief" />
+            </div>
+        </>
     )
 }
 
